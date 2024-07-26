88bd583769665a89d51ef8492c3798a917d6a573
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-07-26 16:08:50 +0530

    Query,Params

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/",(req,res)=>
 {
     res.send("Hello to server")
 })
+app.get('/api',(req,res)=>
+{
+    console.log(req.query.id)
+    res.send(req.query.id)
+})
 app.listen(3000,()=>
 {
     console.log("Server is running on 3000")
